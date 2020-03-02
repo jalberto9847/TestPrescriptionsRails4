eac130f4ff3259f22191a1e30f04f23090e81080
jalberto9847 <josealberto.ch.b@gmail.com>
2020-03-02 10:27:51 -0600

    Added term 'velocity' to describe the rate of task completion

--- gatherer/app/models/project.rb
+++ gatherer/app/models/project.rb
@@ -1,9 +1,10 @@
 class Project
 
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
 
   def initialize
     @tasks = []
+    @due_date = :due_date
   end
 
   def incomplete_tasks
@@ -20,5 +21,21 @@ class Project
 
   def remaining_size
     incomplete_tasks.sum(&:size)
+  end
+
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    (Date.today + projected_days_remaining) <= due_date
   end
 end
