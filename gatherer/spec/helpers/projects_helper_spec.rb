9119a5a16d82d3e765526b112becaef2a1486fe5
jalberto9847 <josealberto.ch.b@gmail.com>
2020-03-09 12:51:39 -0600

    Edited project helper method

--- gatherer/spec/helpers/projects_helper_spec.rb
+++ gatherer/spec/helpers/projects_helper_spec.rb
@@ -8,18 +8,10 @@ RSpec.describe ProjectsHelper, :type => :helper do
     actual = helper.name_with_status(project)
     expect(actual).to have_selector("span.on_schedule", text: "Project Runway")
   end
-end
 
-# Specs in this file have access to a helper object that includes
-# the ProjectsHelper. For example:
-#
-# describe ProjectsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-#RSpec.describe ProjectsHelper, type: :helper do
-#  pending "add some examples to (or delete) #{__FILE__}"
-#end
+  it "augments project name with status info when behind schedule" do
+    allow(project).to receive(:on_schedule?).and_return(false)
+    actual = helper.name_with_status(project)
+    expect(actual).to have_selector("span.behind_schedule", text: "Project Runway")
+  end
+end
