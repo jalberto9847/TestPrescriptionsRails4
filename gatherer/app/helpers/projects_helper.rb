9c96d7d90314f9dc762ff639b400a5edd0babb2b
jalberto9847 <josealberto.ch.b@gmail.com>
2020-03-09 11:12:00 -0600

    Added routing tests / project helper method

--- gatherer/app/helpers/projects_helper.rb
+++ gatherer/app/helpers/projects_helper.rb
@@ -1,2 +1,6 @@
 module ProjectsHelper
+
+  def name_with_status(project)
+    content_tag(:span, project.name, class: 'on_schedule')
+  end
 end
--- gatherer/spec/helpers/projects_helper_spec.rb
+++ gatherer/spec/helpers/projects_helper_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+RSpec.describe ProjectsHelper, :type => :helper do
+  let(:project) { Project.new(name: "Project Runway") }
+
+  it "arguments with status info" do
+    allow(project).to receive(:on_schedule?).and_return(true)
+    actual = helper.name_with_status(project)
+    expect(actual).to have_selector("span.on_schedule", text: "Project Runway")
+  end
+end
+
 # Specs in this file have access to a helper object that includes
 # the ProjectsHelper. For example:
 #
