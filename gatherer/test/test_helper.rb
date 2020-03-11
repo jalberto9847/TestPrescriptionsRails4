0fb2fa2e0f0aca6b760754e19fa567380f91d404
jalberto9847 <josealberto.ch.b@gmail.com>
2020-03-11 09:37:50 -0600

    Minitest Basics
    Include a couple of gem (minitest)
    Create test/models/task_test.rb file

--- gatherer/test/test_helper.rb
+++ gatherer/test/test_helper.rb
@@ -1,13 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require_relative '../config/environment'
+#require File.expand_path('../config/environment',__dir__)
 require 'rails/test_help'
+#require "minitest/rails/capybara"
+#require 'mocha/mini_test'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
